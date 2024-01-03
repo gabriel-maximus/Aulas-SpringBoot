7ca35b105714cecfe965a75d4095ae5a783f9a4b
gabriel-maximus <gabrielmaximus80@gmail.com>
2024-01-02 22:32:39 -0300

    Recurso de adicionar novos usuarios - testado com Postman

--- demo-park-api/src/main/java/com/gabriel/demoparkapi/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/gabriel/demoparkapi/service/UsuarioService.java
@@ -1,12 +1,19 @@
 package com.gabriel.demoparkapi.service;
 
+import com.gabriel.demoparkapi.entity.Usuario;
 import com.gabriel.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- demo-park-api/src/main/java/com/gabriel/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/gabriel/demoparkapi/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.gabriel.demoparkapi.web.controller;
 
+import com.gabriel.demoparkapi.entity.Usuario;
 import com.gabriel.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
