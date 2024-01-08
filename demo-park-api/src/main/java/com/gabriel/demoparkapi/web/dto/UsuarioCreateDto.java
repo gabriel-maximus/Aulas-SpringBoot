8f6a4ae328b9dc209a6367327c092e6b915029dc
gabriel-maximus <gabrielmaximus80@gmail.com>
2024-01-08 19:45:51 -0300

    Adicionando validação nos campos de requisição

--- demo-park-api/src/main/java/com/gabriel/demoparkapi/web/dto/UsuarioCreateDto.java
+++ demo-park-api/src/main/java/com/gabriel/demoparkapi/web/dto/UsuarioCreateDto.java
@@ -1,10 +1,17 @@
 package com.gabriel.demoparkapi.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email(message = "Formato do email inválido", regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
     private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
