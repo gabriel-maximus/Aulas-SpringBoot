a9bf89db5ce2018e51bb73a03d77b9a0bec3e63d
gabriel-maximus <gabrielmaximus80@gmail.com>
2024-01-22 11:52:48 -0300

    Documentando o recurso de criação de novo usuário

--- demo-park-api/src/main/java/com/gabriel/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/gabriel/demoparkapi/web/controller/UsuarioController.java
@@ -6,6 +6,12 @@ import com.gabriel.demoparkapi.web.dto.UsuarioCreateDto;
 import com.gabriel.demoparkapi.web.dto.UsuarioResponseDto;
 import com.gabriel.demoparkapi.web.dto.UsuarioSenhaDto;
 import com.gabriel.demoparkapi.web.dto.mapper.UsuarioMapper;
+import com.gabriel.demoparkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,12 +20,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Usuarios", description = "Contém as operações para cadastro, edição e leitura de um usuário.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
+    @Operation(summary = "Criar um novo usuário.",
+    responses = {
+            @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = UsuarioResponseDto.class))),
+            @ApiResponse(responseCode = "422", description = "Dados de entrada inválidos",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+    })
     @PostMapping
     public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto){
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
